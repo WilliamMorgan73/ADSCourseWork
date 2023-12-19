3b1f3f7c8151452dccff8df3efba5a7e5a276b38
William Morgan <sghp78@durham.ac.uk>
2023-12-19 11:07:52 -0500

    Finished q7

--- q7.py
+++ q7.py
@@ -4,7 +4,29 @@ def Merge3Way(L1,L2,L3):
     in non-increasing order into a new list L that is
     sorted in non-increasing order, and return L """
 
-    #Non increasing means every time after words is less than or equal to
+    L = []
+
+    while L1 or L2 or L3:
+        # Get the minimum value among the current elements from L1, L2, and L3
+        min_val = min(L1[-1] if L1 else float('inf'),
+                      L2[-1] if L2 else float('inf'),
+                      L3[-1] if L3 else float('inf'))
+
+        # Append the minimum value to the L list
+        L.append(min_val)
+
+        # Pop the element from the list where the minimum value was found
+        if L1 and min_val == L1[-1]:
+            L1.pop()
+        elif L2 and min_val == L2[-1]:
+            L2.pop()
+        elif L3 and min_val == L3[-1]:
+            L3.pop()
+
+    return L[::-1]
+
+
+
     
 def Merge3Sort(L):
     """ If L has at most three elements, sort L into
@@ -14,15 +36,28 @@ def Merge3Sort(L):
     to merge them. In either case, return a list
     that is a sorted version of L """
     
-# Test for Merge3Way
-L1 = [8,6,6,4]
-L2 = [10,7,2,1]
-L3 = [9,5,2]
-assert(Merge3Way(L1,L2,L3)==[10,9,8,7,6,6,5,4,2,2,1])
-print("Merge3Way test passed")
+    
+    if len(L) <= 3:
+        # Base case: if L has at most three elements, sort it directly
+        for i in range(1, len(L)):
+            currentElement = L[i]
+            j = i - 1
+            while j >= 0 and currentElement > L[j]:
+                L[j + 1] = L[j]
+                j -= 1
+            L[j + 1] = currentElement
+        return L
+    else:
+        # Recursive case: split the list into three sub-lists
+        length = len(L)
+        marker1 = length // 3
+        marker2 = (2 * length) // 3
 
-# Test for Merge3Sort
-L = [5,19,14,13,2,8,5,19,6]
-assert(Merge3Sort(L)==[19,19,14,13,8,6,5,5,2])
-print("Merge3Sort test passed")
+        # Make three recursive calls on sub-lists
+        L1 = Merge3Sort(L[:marker1])
+        L2 = Merge3Sort(L[marker1:marker2])
+        L3 = Merge3Sort(L[marker2:])
 
+        # Merge the three sorted sub-lists using Merge3Way
+        return Merge3Way(L1, L2, L3)
+    
