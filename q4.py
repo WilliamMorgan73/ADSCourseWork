02a0827918cc84fe4067f69e656f3cfb45ff2fd4
William Morgan <sghp78@durham.ac.uk>
2023-12-20 12:54:09 -0500

    Code does every starting permutation, however there are multiple possible solutions for each starting permutation

--- q4.py
+++ q4.py
@@ -1,17 +1,20 @@
 from itertools import permutations
 
-def is_valid_placement(matrix, row, col, permutation):
-    # Check if each value of the permutation is unique in the current row and column
-    
-    for i in range(row):
-        #Check if the current value is unique in the current row
-        
-        if matrix[row][i] == permutation[col]:
+def is_valid_placement(matrix, row, permutation):
+    # Check if each value of the permutation is unique in the current row and the respective column
+    for i in range(len(permutation)):
+        # Check if the current value is unique in the current row
+        if permutation[i] in matrix[row]:
             return False
-        #Check if the current value is unique in the current column
-        if matrix[i][col] == permutation[col]:
-            return False
+
+        # Check if the current value is unique in the respective column
+        for j in range(row):
+            if permutation[i] == matrix[j][i]:
+                return False
+            
     return True
+    
+    
 
 def generateRow(matrix, row, n):
     if row == n:
@@ -19,21 +22,18 @@ def generateRow(matrix, row, n):
 
     #Generate all permutations of the numbers from 1 to n
     for permutation in permutations(range(1, n + 1)):
-        #Attempt to fill the current row with the current permutation
-        for i in range(n):
-            # Check if the current permutation is a valid placement
-            if is_valid_placement(matrix, row, i, list(permutation)):
-                # Valid placement, fill the current row with the current permutation
-                matrix[row] = list(permutation)
-
-                break
+        # Check if the current permutation is a valid placement
+        if is_valid_placement(matrix, row, list(permutation)):
+            # Valid placement, fill the current row with the current permutation
+            matrix[row] = list(permutation)
+            break
             
-            #If the current permutation is not a valid placement, try the next permutation
-            else:
-                continue
+        #If the current permutation is not a valid placement, try the next permutation
+        else:
+            continue
             
-            #If all permutations have been tried and no valid placement is found, backtrack, unless the current row is the second row
-            
+        #If all permutations have been tried and no valid placement is found, backtrack, unless the current row is the second row
+        return False
     
 
 def generateIndividualBruckner(n, permutation):
@@ -52,8 +52,7 @@ def generateIndividualBruckner(n, permutation):
 
         else:
             # Backtrack if no valid value found for the current row
-            for i in range(row, n):
-                output[i][i] = 0
+            row = 1
 
     return output
 
@@ -64,11 +63,15 @@ def generate_bruckner(n):
     for permutation in permutations(range(1, n + 1)):
         # Generate the Bruckner matrix for the current permutation and add it to the result
         brucknerMatrix = generateIndividualBruckner(n, permutation)
+
+        
         result.append(brucknerMatrix)
 
     return result
 
 # Example: Generate all Bruckner matrices of size 4
-bruckner_matrices_4 = generate_bruckner(4)
+bruckner_matrices_4 = generate_bruckner(5)
 for brucknerMatrix in bruckner_matrices_4:
     print(brucknerMatrix)
+    
+print(len(bruckner_matrices_4))
